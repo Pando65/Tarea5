26a10d9d80ee7a6d10beb05a878a21d373ba4653
Omar Manjarrez <omjrrz@outlook.com>
2015-02-18 18:29:37 -0600

    cambios de standar a tarea5.java

--- src/tarea5/Tarea5.java
+++ src/tarea5/Tarea5.java
@@ -1,17 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * Tarea 5
+ *
+ * JFrame que ejecuta el Juego
+ *
+ * @author Omar Manjarrez & Jose Manuel Gonzalez 
+ * @matriculas A00815248 & A01280106
+ * @version 1.0
+ * @date 17/02/15
+ */ 
 package tarea5;
 
-/**
- *
- * @author omjrrz
- */
-
 public class Tarea5  {
-
+    
     public static void main(String[] args) {
         //Crea un nuevo objeto JFrameHolaMundo
         Juego jueJuego = new Juego();
